17f7cf3c9a1d26cc572ade36406180fd88de85e4
Hiroyuki Xiong <xionghiroyuki@gmail.com>
2019-05-10 19:19:32 -0700

    Integrated firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from '../environments/environment';
+import { AngularFireStorageModule } from 'angularfire2/storage';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -8,6 +13,9 @@ import { ContactComponent } from './components/contact/contact.component';
 import { AppRoutingModule } from './app-routing.module';
 import { PetsComponent } from './components/pets/pets.component';
 import { PetBioComponent } from './components/pet-bio/pet-bio.component';
+
+import { FirebaseService } from './services/firebase.service';
+import { AngularFireDatabase } from 'angularfire2/database';
 
 @NgModule({
   declarations: [
@@ -20,9 +28,13 @@ import { PetBioComponent } from './components/pet-bio/pet-bio.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    AngularFireStorageModule
   ],
-  providers: [],
+  providers: [FirebaseService, AngularFireDatabase],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyA-vdHaIPRkl7VuMrFPv39TMNIu_8rIxgw",
+    authDomain: "pet-fosters-database.firebaseapp.com",
+    databaseURL: "https://pet-fosters-database.firebaseio.com",
+    projectId: "pet-fosters-database",
+    storageBucket: "pet-fosters-database.appspot.com",
+    messagingSenderId: "388160439057",
+    appId: "1:388160439057:web:2a9ae97396e4d159"
+  }
 };
 
 /*
